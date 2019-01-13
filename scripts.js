d0ea8b4fafb2237fbb786fd76c12f3e1e4bd438f
ErikBoesen <me@erikboesen.com>
2019-01-13 12:16:21 -0500

    Make custom popups show up

--- scripts.js
+++ scripts.js
@@ -5,80 +5,12 @@ var logos = {
     'University of Virginia': 'https://d9tyu2epg3boq.cloudfront.net/institutions/uva.jpg',
 };
 
-var map, popup;
+var map, popup, Popup;
 
 // Called by Maps API upon loading.
 function initMap() {
-    /**
-     * A customized popup on the map.
-     * @param {!google.maps.LatLng} position
-     * @param {!Element} content
-     * @constructor
-     * @extends {google.maps.OverlayView}
-     */
-    Popup = function(position, content) {
-        this.position = position;
+    definePopupClass();
 
-        content.classList.add('popup-bubble-content');
-
-        var pixelOffset = document.createElement('div');
-        pixelOffset.classList.add('popup-bubble-anchor');
-        pixelOffset.appendChild(content);
-
-        this.anchor = document.createElement('div');
-        this.anchor.classList.add('popup-tip-anchor');
-        this.anchor.appendChild(pixelOffset);
-
-        // Optionally stop clicks, etc., from bubbling up to the map.
-        this.stopEventPropagation();
-    };
-    // NOTE: google.maps.OverlayView is only defined once the Maps API has
-    // loaded. That is why Popup is defined inside initMap().
-    Popup.prototype = Object.create(google.maps.OverlayView.prototype);
-
-    /** Called when the popup is added to the map. */
-    Popup.prototype.onAdd = function() {
-        this.getPanes().floatPane.appendChild(this.anchor);
-    };
-
-    /** Called when the popup is removed from the map. */
-    Popup.prototype.onRemove = function() {
-        if (this.anchor.parentElement) {
-            this.anchor.parentElement.removeChild(this.anchor);
-        }
-    };
-
-    /** Called when the popup needs to draw itself. */
-    Popup.prototype.draw = function() {
-        var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-        // Hide the popup when it is far out of view.
-        var display =
-            Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
-            'block' :
-            'none';
-
-        if (display === 'block') {
-            this.anchor.style.left = divPosition.x + 'px';
-            this.anchor.style.top = divPosition.y + 'px';
-        }
-        if (this.anchor.style.display !== display) {
-            this.anchor.style.display = display;
-        }
-    };
-
-    /** Stops clicks/drags from bubbling up to the map. */
-    Popup.prototype.stopEventPropagation = function() {
-        var anchor = this.anchor;
-        anchor.style.cursor = 'auto';
-
-        ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
-            'pointerdown']
-            .forEach(function(event) {
-                anchor.addEventListener(event, function(e) {
-                    e.stopPropagation();
-                });
-            });
-    };
     map = new google.maps.Map(document.getElementById('map'), { // Define Map Settings
         center: {
             lat: 35,
@@ -213,8 +145,82 @@ function initMap() {
     });
 }
 
+function definePopupClass() {
+    /**
+     * A customized popup on the map.
+     * @param {!google.maps.LatLng} position
+     * @param {!Element} content
+     * @constructor
+     * @extends {google.maps.OverlayView}
+     */
+    Popup = function(position, content) {
+        this.position = position;
+
+        content.classList.add('popup-bubble-content');
+
+        var pixelOffset = document.createElement('div');
+        pixelOffset.classList.add('popup-bubble-anchor');
+        pixelOffset.appendChild(content);
+
+        this.anchor = document.createElement('div');
+        this.anchor.classList.add('popup-tip-anchor');
+        this.anchor.appendChild(pixelOffset);
+
+        // Optionally stop clicks, etc., from bubbling up to the map.
+        this.stopEventPropagation();
+    };
+    // NOTE: google.maps.OverlayView is only defined once the Maps API has
+    // loaded. That is why Popup is defined inside initMap().
+    Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+
+    /** Called when the popup is added to the map. */
+    Popup.prototype.onAdd = function() {
+        this.getPanes().floatPane.appendChild(this.anchor);
+    };
+
+    /** Called when the popup is removed from the map. */
+    Popup.prototype.onRemove = function() {
+        if (this.anchor.parentElement) {
+            this.anchor.parentElement.removeChild(this.anchor);
+        }
+    };
+
+    /** Called when the popup needs to draw itself. */
+    Popup.prototype.draw = function() {
+        var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+        // Hide the popup when it is far out of view.
+        var display =
+            Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+            'block' :
+            'none';
+
+        if (display === 'block') {
+            this.anchor.style.left = divPosition.x + 'px';
+            this.anchor.style.top = divPosition.y + 'px';
+        }
+        if (this.anchor.style.display !== display) {
+            this.anchor.style.display = display;
+        }
+    };
+
+    /** Stops clicks/drags from bubbling up to the map. */
+    Popup.prototype.stopEventPropagation = function() {
+        var anchor = this.anchor;
+        anchor.style.cursor = 'auto';
+
+        ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
+            'pointerdown']
+            .forEach(function(event) {
+                anchor.addEventListener(event, function(e) {
+                    e.stopPropagation();
+                });
+            });
+    };
+}
+
+
 function details(institution) {
-    if (popup) popup.close();
+    //if (popup) popup.close();
     var institutionLogo = document.createElement('img'),
         institutionType = document.createElement('p'),
         institutionName = document.createElement('p');
@@ -222,6 +228,7 @@ function details(institution) {
     institutionName.textContent = institution.name;
     institutionType.textContent = institution.type;
     var info = document.createElement('div');
+    info.id = 'info';
     info.appendChild(institutionLogo);
     info.appendChild(institutionName);
     info.appendChild(institutionType);
@@ -239,8 +246,6 @@ function details(institution) {
         studentContainer.appendChild(studentMajor);
         info.appendChild(studentContainer);
     }
-    popup = new google.maps.InfoWindow({
-        content: info.innerHTML,
-    });
-    popup.open(map, institution);
+    popup = new Popup(new google.maps.LatLng(institution.position.lat(), institution.position.lng()), info);
+    popup.setMap(map);
 }
