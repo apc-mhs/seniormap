bc0a29366533a9dd0235390b13f0b347c2319240
ErikBoesen <me@erikboesen.com>
2019-01-13 12:26:38 -0500

    Split popup JS into separate file

--- scripts.js
+++ scripts.js
@@ -145,69 +145,6 @@ function initMap() {
     });
 }
 
-function definePopupClass() {
-    /**
-     * A customized popup on the map.
-     * @param {!google.maps.LatLng} position
-     * @param {!Element} content
-     * @constructor
-     * @extends {google.maps.OverlayView}
-     */
-    Popup = function(position, content) {
-        this.position = position;
-
-        content.classList.add('popup-bubble-content');
-
-        var pixelOffset = document.createElement('div');
-        pixelOffset.classList.add('popup-bubble-anchor');
-        pixelOffset.appendChild(content);
-
-        this.anchor = document.createElement('div');
-        this.anchor.classList.add('popup-tip-anchor');
-        this.anchor.appendChild(pixelOffset);
-
-        // Optionally stop clicks, etc., from bubbling up to the map.
-        this.stopEventPropagation();
-    };
-    // NOTE: google.maps.OverlayView is only defined once the Maps API has
-    // loaded. That is why Popup is defined inside initMap().
-    Popup.prototype = Object.create(google.maps.OverlayView.prototype);
-
-    /** Called when the popup is added to the map. */
-    Popup.prototype.onAdd = function() {
-        this.getPanes().floatPane.appendChild(this.anchor);
-    };
-
-    /** Called when the popup is removed from the map. */
-    Popup.prototype.onRemove = function() {
-        if (this.anchor.parentElement) {
-            this.anchor.parentElement.removeChild(this.anchor);
-        }
-    };
-
-    /** Called when the popup needs to draw itself. */
-    Popup.prototype.draw = function() {
-        var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
-        this.anchor.style.left = divPosition.x + 'px';
-        this.anchor.style.top = (divPosition.y-50) + 'px';
-    };
-
-    /** Stops clicks/drags from bubbling up to the map. */
-    Popup.prototype.stopEventPropagation = function() {
-        var anchor = this.anchor;
-        anchor.style.cursor = 'auto';
-
-        ['click', 'dblclick', 'contextmenu', 'wheel', 'mousedown', 'touchstart',
-            'pointerdown']
-            .forEach(function(event) {
-                anchor.addEventListener(event, function(e) {
-                    e.stopPropagation();
-                });
-            });
-    };
-}
-
-
 function details(institution) {
     //if (popup) popup.close();
     var institutionLogo = document.createElement('img'),
@@ -217,7 +154,7 @@ function details(institution) {
     institutionName.textContent = institution.name;
     institutionType.textContent = institution.type;
     var info = document.createElement('div');
-    info.id = 'info';
+    info.className = 'info';
     info.appendChild(institutionLogo);
     info.appendChild(institutionName);
     info.appendChild(institutionType);
