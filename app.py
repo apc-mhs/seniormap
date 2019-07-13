d69620443ec1050bdf78de7972a8e3824f0da78f
ErikBoesen <me@erikboesen.com>
2019-07-12 20:20:58 -0400

    Add SQL interaction

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# Suppress warnings
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
 @app.route("/")
