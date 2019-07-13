756fede6638412ba93477970df176c7f17849cf9
ErikBoesen <me@erikboesen.com>
2019-07-13 08:11:18 -0400

    Add blacklisting to image collation script

--- collate.py
+++ collate.py
@@ -2,7 +2,17 @@ import wikipedia
 with open("universities.txt", "r") as f:
     universities = f.readlines()
 
+blacklist = (
+    "Flag_of",
+    "Text_document_with_red_question_mark",
+    "_Map_",
+    "Graduation_hat",
+    "Question_book",
+    "Andorra_Telecom_2019_logo",
+)
+
 def get_image(options):
+    options = [option for option in options if not any(x in image for x in blacklist)]
     if not options:
         return ""
     for image in options:
@@ -18,6 +28,7 @@ with open("universities.csv", "w") as f:
             print("Processing " + university)
             try:
                 page = wikipedia.page(university)
+                print(page.title)
                 image = get_image(page.images)
             except KeyError:
                 image = ""
