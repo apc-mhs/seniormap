9afe5f1843fefd867f5fb40fd7a7f153d27a3923
ErikBoesen <me@erikboesen.com>
2019-08-09 18:04:33 -0400

    Use sqlite by default

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///" + ospath.join(basedir, "app.db"))
 # Suppress warnings
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
