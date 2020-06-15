28b0c33299dca8fa25c59e7af985c31494853c91
AndrewLester <alester220@gmail.com>
2020-06-14 23:26:38 -0400

    Make certain attributes non-null for the School model

--- app/map/models.py
+++ app/map/models.py
@@ -3,10 +3,10 @@ from app.exts import db
 
 class School(db.Model):
     __tablename__ = 'schools'
-    slug = db.Column(db.String(16), unique=True, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-    form = db.Column(db.String(32), unique=True)
-    sheet = db.Column(db.String(64), unique=True)
+    slug = db.Column(db.String(16), primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+    form = db.Column(db.String(32), unique=True, nullable=False)
+    sheet = db.Column(db.String(64), unique=True, nullable=False)
     background = db.Column(db.String(16))
     light_background = db.Column(db.String(16))
     lighter_background = db.Column(db.String(16))
